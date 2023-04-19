3dc69ea02278f63485302c239aebb416227153c7
ahmadysalah <a.ahmad.salah@gmail.com>
2023-04-19 11:28:36 +0200

    fix

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,13 +1,15 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class HomeController {
 
     @GetMapping(path = "/")
     public String getHello(){
-        return "Hello World new";
+        return "ahmad and abd new";
     }
 }
